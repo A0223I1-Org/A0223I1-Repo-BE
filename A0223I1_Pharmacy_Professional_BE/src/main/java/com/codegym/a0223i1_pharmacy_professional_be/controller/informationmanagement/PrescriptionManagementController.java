712bf935fb33c0f270e5563f93fa06aafa042e1d
HauTrinh <hautrinh242@gmail.com>
2024-02-19 15:28:06 +0700

    #1-HauTCCreateRpo

---
+++ A0223I1_Pharmacy_Professional_BE/src/main/java/com/codegym/a0223i1_pharmacy_professional_be/controller/informationmanagement/PrescriptionManagementController.java
@@ -0,0 +1,8 @@
+package com.codegym.a0223i1_pharmacy_professional_be.controller.informationmanagement;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrescriptionManagementController {
+    // QUản lý thông tin toa thuốc
+}
