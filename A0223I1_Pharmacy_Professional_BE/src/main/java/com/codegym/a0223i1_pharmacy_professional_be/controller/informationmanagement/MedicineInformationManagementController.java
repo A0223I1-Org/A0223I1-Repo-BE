74583d52d130d4597087985783870d17a85e3559
lequangtrieu <trieulqde160447@gmail.com>
2024-02-23 20:31:51 +0700

    up MedicineController

--- A0223I1_Pharmacy_Professional_BE/src/main/java/com/codegym/a0223i1_pharmacy_professional_be/controller/informationmanagement/MedicineInformationManagementController.java
+++ A0223I1_Pharmacy_Professional_BE/src/main/java/com/codegym/a0223i1_pharmacy_professional_be/controller/informationmanagement/MedicineInformationManagementController.java
@@ -1,8 +1,42 @@
 package com.codegym.a0223i1_pharmacy_professional_be.controller.informationmanagement;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codegym.a0223i1_pharmacy_professional_be.entity.Medicine;
+import com.codegym.a0223i1_pharmacy_professional_be.service.interfaceservice.informationmanagement.MedicineInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/medicine")
+@CrossOrigin("*")
 public class MedicineInformationManagementController {
-    // Quản lý thông tin thuốc
+    @Autowired
+    MedicineInformationService medicineService;
+
+    @GetMapping()
+    public ResponseEntity<List<Medicine>> getAllMedicine() {
+        return new ResponseEntity<>(medicineService.getAllMedicine(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Medicine> getMedicineById(@PathVariable String id) {
+        return new ResponseEntity<>(medicineService.findMedicineById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMedicineById(@PathVariable String id) {
+        System.out.println(id);
+
+        Medicine existingMedicine = medicineService.findMedicineById(id);
+
+        if (existingMedicine == null) {
+            return new ResponseEntity<>("Không tìm thấy Medicine có ID: " + id, HttpStatus.NOT_FOUND);
+        }
+
+        medicineService.deleteMedicine(existingMedicine);
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
 }
