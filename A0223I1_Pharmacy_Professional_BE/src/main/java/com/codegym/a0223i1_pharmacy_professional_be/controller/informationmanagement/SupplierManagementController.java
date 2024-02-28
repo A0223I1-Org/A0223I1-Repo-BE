2b993d1427ef4c4356810e5a401e54ca9cbc7d58
vankhang3006 <119319324+vankhang3006@users.noreply.github.com>
2024-02-28 19:48:01 +0700

    #6.11.4 - KhangNV CRUDSupplier CreateAPI

--- A0223I1_Pharmacy_Professional_BE/src/main/java/com/codegym/a0223i1_pharmacy_professional_be/controller/informationmanagement/SupplierManagementController.java
+++ A0223I1_Pharmacy_Professional_BE/src/main/java/com/codegym/a0223i1_pharmacy_professional_be/controller/informationmanagement/SupplierManagementController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/suppliers")
 public class SupplierManagementController {
     //Quản lý nhà cung cấp
